ff1685f47e2aef145c9a4148c2b7d3e5e672af60
sunwei <sunwayliving@gmail.com>
2014-08-27 12:55:51 +0800

    finish sort and binary search

---
+++ sunwei/Search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int *a, int start, int end, int target){
+    if(a == NULL) return -1;
+    int l = start, r = end;
+    while(l < r){
+        const int mid = l + ((r - l) >> 1);
+        if(a[mid] == target){
+            return mid;
+        }else if(a[mid] > target){
+            r = mid;
+        }else{
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int a[] = {0, 1, 4, 5, 6, 7, 8, 9};
+    const int n = sizeof(a) / sizeof(a[0]);
+    cout<<binary_search(a, 0, n, 4) <<endl;
+    return 0;
+}
