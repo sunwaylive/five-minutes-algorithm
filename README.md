fbf95fd6b4b9384fe1cac3daab66be9e1bc1a3df
sunwei <sunwayliving@gmail.com>
2014-09-26 21:53:26 +0800

    add search_in_rotated_array and modify README.md

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ five-minutes-algorithms
 Basic algorithms everybody should solve in 5 minutes
 
 ====================
-1. Make a directory in the root folder, named with your own real name, all letters non capital
-
-2. Implement the algorithm using your best codes and then make a PR, first letter of each word of file name should be capital 
--- Search.cpp
+++ Search.cpp
@@ -18,6 +18,32 @@ int binary_search(int *a, int start, int end, int target){
 }
 
 //************************************************************
+int binary_search_rotated(int *a, int start, int end, int target){
+    if(a == NULL) return -1;
+
+    int l = start, r = end;
+    while(l < r){
+        int m = l + ((r - l) >> 1);
+        if(a[m] == target) return m;
+
+        if(a[m] >= a[l]){
+            if(target >= a[l] && target <= a[m]){
+                r = m;
+            }else{
+                l = m + 1;
+            }
+        }else if(a[m] <= a[r - 1]){
+            if(target >= a[m] && target <= a[r - 1]){
+                l = m + 1;
+            }else{
+                r = m;
+            }
+        }
+    }
+    return -1;
+}
+
+//************************************************************
 int findKth(int *a, int m, int a_start, int *b, int n, int b_start, int k){
     if(a_start >= m) return b[b_start + k - 1];
     if(b_start >= n) return a[a_start + k - 1];
@@ -46,12 +72,11 @@ double findMedianSortedArrays(int *a, int m, int *b, int n){
 
 int main()
 {
-    int a[] = {0, 1, 4, 5, 6, 7, 8};
-    int b[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    const int m = sizeof(a) / sizeof(a[0]);
-    const int n = sizeof(b) / sizeof(b[0]);
-    cout<<m<<" "<<n<<endl;
-    cout<<findMedianSortedArrays(a, m, b, n)<<endl;;
-    cout<<binary_search(a, 0, n, 4) <<endl;
+    int a[] = {4, 5, 6, 1, 2, 3};
+    int n = sizeof(a) / sizeof(*a);
+    cout<<binary_search_rotated(a, 0, n, 14) <<endl;
+    char s[] = "abcde";
+    cout<<strlen(s)<<endl;
+    cout<<sizeof(s) <<endl;
     return 0;
 }
