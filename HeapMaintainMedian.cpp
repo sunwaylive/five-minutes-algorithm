88aa8fdeb769a694cb4cb1213e01137362d49667
sunway <sunwayliving@gmail.com>
2016-10-08 00:14:47 +0800

    Forget to git add this file

--- HeapMaintainMedian.cpp
+++ HeapMaintainMedian.cpp
@@ -3,44 +3,42 @@
 
 using namespace std;
 
-int main()
-{
-    priority_queue<int, vector<int>, less<int> > max_heap;
-    priority_queue<int, vector<int>, greater<int> > min_heap;
-    int x;
-    double mid = INT_MAX;
-    while(cin>>x && x){
-        if(mid == INT_MAX){
-            max_heap.push(x);
-            mid = x;
-            cout<<"mid is: " <<mid <<endl;
-            continue;
-        }
+class MidFinder {
+public:
+    void AddNum(int num) {
+        // the size of max heap doesn't change, min heap size add one
+        // cool codes!
+        m_max_heap.push(num);
+        m_min_heap.push(m_max_heap.top());
+        m_max_heap.pop();
 
-        if(x <= mid) max_heap.push(x);
-        else min_heap.push(x);
-
-        int left = max_heap.size();
-        int right = min_heap.size();
-        int tmp = 0;
-
-        if(left == right){
-            mid = ((max_heap.top() + min_heap.top()) / 2.0f);
-            cout<<"mid is: " <<mid <<endl;
-        }else if(abs(left - right) == 1){
-            tmp = (left > right) ? max_heap.top() : min_heap.top();
-            mid = tmp;
-            cout<<"mid is: " <<mid <<endl;
-        }else if(left - right >= 2){
-            tmp = max_heap.top(); max_heap.pop();
-            min_heap.push(tmp);
-            mid = ((max_heap.top() + min_heap.top()) / 2.0f);
-            cout<<"mid is: " <<mid <<endl;
-        }else if(right - left >= 2){
-            tmp = min_heap.top(); min_heap.pop();
-            max_heap.push(tmp);
-            mid = ((max_heap.top() + min_heap.top()) / 2.0f);
-            cout<<"mid is: " <<mid <<endl;
+        if (m_min_heap.size() > m_max_heap.size()) {
+            m_max_heap.push(m_min_heap.top());
+            m_min_heap.pop();
         }
     }
+
+    float FindMedian() {
+        if (m_max_heap.size() == m_min_heap.size()) {
+            return (m_max_heap.top() + m_min_heap.top()) / 2.0f;
+        } else {
+            // only one possibility, min heap size is one larger
+            return m_min_heap.top();
+        }
+    }
+
+private:
+    priority_queue<int> m_max_heap;
+    priority_queue<int, vector<int>, greater<int> > m_min_heap;
+};
+
+int main() {
+    MidFinder mf;
+    mf.AddNum(2);
+    mf.AddNum(3);
+    cout << mf.FindMedian() << endl;
+    mf.AddNum(4);
+    cout << mf.FindMedian() << endl;
+    return 0;
 }
+
